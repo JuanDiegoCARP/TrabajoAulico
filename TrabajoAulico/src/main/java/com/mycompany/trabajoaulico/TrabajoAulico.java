91478edd2785743d7956826f4ec8d13643560868
JuanDiegoCARP <163307155+JuanDiegoCARP@users.noreply.github.com>
2024-03-21 11:54:21 -0300

    Delete TrabajoAulico directory

--- TrabajoAulico/src/main/java/com/mycompany/trabajoaulico/TrabajoAulico.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.trabajoaulico;
-public class TrabajoAulico {
-
-    
-    
-    public static void main(String[] args) {
-        System.out.println("Trabajo Practico");
-    }
-}
