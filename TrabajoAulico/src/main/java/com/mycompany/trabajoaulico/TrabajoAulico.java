ec0ef885878787de2f5b131746729a00825591bc
JuanDiegoCARP <familiaalonso.jda@gmail.com>
2024-03-21 09:53:08 -0300

    Archivo para trabajar

---
+++ TrabajoAulico/src/main/java/com/mycompany/trabajoaulico/TrabajoAulico.java
@@ -0,0 +1,9 @@
+package com.mycompany.trabajoaulico;
+public class TrabajoAulico {
+
+    
+    
+    public static void main(String[] args) {
+        System.out.println("Trabajo Practico");
+    }
+}
